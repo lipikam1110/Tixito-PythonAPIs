136524d24f5c7bade13541304f085eee6ab9d586
Aeklavya17 <aeklavya.rajgor@senkusha.in>
2024-06-08 20:29:53 +0530

    Updated Codes

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from sqlalchemy import CheckConstraint
 class User(db.Model):
     __tablename__ = 'app_users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
+    name = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     mobile = db.Column(db.String(15), unique=True, nullable=False)
     whatsapp_notification_enabled = db.Column(db.Boolean, default=False, nullable=False)
