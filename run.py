083ecde387fefac14f4262e227f4fd214c0c56a2
Aeklavya17 <aeklavya.rajgor@senkusha.in>
2024-06-11 20:02:36 +0530

    Updated Port

--- run.py
+++ run.py
@@ -2,5 +2,12 @@ from app import create_app
 
 app = create_app()
 
+# if __name__ == '__main__':
+#     app.run(debug=True, host='127.0.0.1', port=)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)  # Use a different port number, e.g., 5001
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
